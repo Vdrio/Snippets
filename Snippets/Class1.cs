09d62afb02002271fbda86a357209eab6d47b8a9
Vdrio <40824150+Vdrio@users.noreply.github.com>
2020-09-17 14:52:46 -0700

    Add project files.

---
+++ Snippets/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+
+namespace Snippets
+{
+    public class Class1
+    {
+
+
+        public const string defaultUri = "https://localhost:5001/";
+        public static CookieContainer Cookies = new CookieContainer();
+        HttpClient GetClient()
+        {
+            HttpClient _client;
+            _client = new HttpClient(new HttpClientHandler() { CookieContainer = Cookies })
+            {
+                BaseAddress = new Uri(defaultUri),
+            };
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.Timeout = TimeSpan.FromMilliseconds(10000);
+
+            return _client;
+        }
+
+
+    }
+}
